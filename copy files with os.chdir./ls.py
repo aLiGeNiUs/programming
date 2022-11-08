133053f3c2e9fcbb53473368f1cd8f6a8a5bdd72
aLi GeNiUs <aligeniusnet@gmail.com>
2022-11-08 23:33:50 +0300

    Update ls.py

--- copy files with os.chdir./ls.py
+++ copy files with os.chdir./ls.py
@@ -1,1 +1,9 @@
-
+import os
+dir1 = '/home/x/TestCode/dir1'
+dir2 = '/home/x/TestCode/dir2'
+os.chdir(dir1)
+print(os.getcwd())
+os.system('cp *.jpg /home/x/TestCode/dir2/ ')
+os.chdir(dir2)
+print(os.getcwd())
+print(os.listdir(dir2))
